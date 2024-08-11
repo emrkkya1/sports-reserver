cbef9a8632b34ab86f8c857c6a241ae9effec00e
b2220356079 <b2220356079@cs.hacettepe.edu.tr>
2024-08-12 01:33:44 +0300

    ReservationResult was created, ReservationController and ReservationService were implemented.

--- src/main/java/org/ohaust/springwebdemo/controller/ReservationController.java
+++ src/main/java/org/ohaust/springwebdemo/controller/ReservationController.java
@@ -1,24 +1,28 @@
 package org.ohaust.springwebdemo.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.ohaust.springwebdemo.model.ReservableDate;
+import org.ohaust.springwebdemo.model.request.ReservationRequest;
+import org.ohaust.springwebdemo.model.result.ReservationResult;
 import org.ohaust.springwebdemo.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 public class ReservationController {
-    @Autowired
-    ReservationService service;
+
+    private final ReservationService reservationService;
 
 
-    /*
     @PostMapping("/reserve")
-    public void reserveHour(@RequestBody ReservableDate reservableDate) {
-        service.reserveDate(reservableDate);
-        System.out.println("Hour" + reservableDate + "successfully reserved");
+    public ResponseEntity<String> reserve(@RequestBody ReservationRequest reservationRequest) {
+        ReservationResult reservationResult = reservationService.reserveIfAvailable(reservationRequest);
+        if (reservationResult.isSuccess()) {
+            return ResponseEntity.ok(reservationResult.getMessage());
+        }
+        return ResponseEntity.badRequest().body(reservationResult.getMessage());
     }
-    */
-
-
 
 }
--- src/main/java/org/ohaust/springwebdemo/service/ReservationService.java
+++ src/main/java/org/ohaust/springwebdemo/service/ReservationService.java
@@ -1,21 +1,66 @@
 package org.ohaust.springwebdemo.service;
 
+import org.ohaust.springwebdemo.model.Date;
 import org.ohaust.springwebdemo.model.ReservableDate;
+import org.ohaust.springwebdemo.model.ReservableHour;
+import org.ohaust.springwebdemo.model.request.ReservationRequest;
+import org.ohaust.springwebdemo.model.result.ReservationResult;
 import org.ohaust.springwebdemo.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReservationService {
 
-    @Autowired
-    ReservationRepository repository;
+    private final ReservationRepository repository;
 
-    public ReservationService() {
 
+    public ReservationService(ReservationRepository repository) {
+        this.repository = repository;
+    }
+
+    public ReservationResult reserveIfAvailable(ReservationRequest reservationRequest) {
+        Date date = reservationRequest.getDate();
+
+        ReservableDate dateToReserve = repository.findByDate(date);
+        if (dateToReserve == null) {
+            return new ReservationResult(false,"Date does not exists or not available.");
+        }
+        int hour = reservationRequest.getHour();
+        List<ReservableHour> reservableHourList = dateToReserve.getReservableHourList();
+        int index = findIndexOfHour(reservableHourList, hour);
+        if (index == -1) {
+            return new ReservationResult(false,"Hour is not available for reservation.");
+        }
+        ReservableHour reservableHour = reservableHourList.get(index);
+        if (reserve(reservableHour)) {
+            repository.save(dateToReserve);
+            return new ReservationResult(true,"Successfully reserved hour.");
+
+        }
+        return new ReservationResult(false,"Hour already reserved!");
+    }
+
+    public boolean reserve(ReservableHour reservableHour) {
+        if (reservableHour.isReserved()) {
+            return false;
+        }
+        reservableHour.setReserved(true);
+        return true;
     }
 
 
+    private int findIndexOfHour(List<ReservableHour> reservableHourList, int hour) {
+        for (int i = 0; i < reservableHourList.size(); i++) {
+            int hourOfDay = reservableHourList.get(i).getHour();
+            if (hour == hourOfDay) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 
 }
