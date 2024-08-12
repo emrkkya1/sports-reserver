ec10ea1c5c139e594e63d25d6d0a4425ee650a66
b2220356079 <b2220356079@cs.hacettepe.edu.tr>
2024-08-12 15:27:35 +0300

    Removed 2 unused methods.

--- src/main/java/org/ohaust/springwebdemo/model/TimePoint.java
+++ src/main/java/org/ohaust/springwebdemo/model/TimePoint.java
@@ -12,17 +12,4 @@ public class TimePoint {
     private int hour;
     private int minute;
 
-    public TimePoint getPrev() {
-        if (minute < 15) {
-            return new TimePoint(hour - 1, minute + 45);
-        }
-        return new TimePoint(hour, minute - 15);
-    }
-
-    public TimePoint getNext() {
-        if (minute > 45) {
-            return new TimePoint(hour + 1, minute - 45);
-        }
-        return new TimePoint(hour, minute + 15);
-    }
 }
