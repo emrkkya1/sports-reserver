0b3ddb31e2b2adb7b69186dff018571baf67b7c6
b2220356079 <b2220356079@cs.hacettepe.edu.tr>
2024-08-13 19:10:16 +0300

    checkstyle

--- src/main/java/org/ohaust/springwebdemo/service/AvailabilityService.java
+++ src/main/java/org/ohaust/springwebdemo/service/AvailabilityService.java
@@ -4,7 +4,6 @@ package org.ohaust.springwebdemo.service;
 import lombok.RequiredArgsConstructor;
 import org.ohaust.springwebdemo.model.ReservableDate;
 import org.ohaust.springwebdemo.repository.ReservationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
